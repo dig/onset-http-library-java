2220857634832a2ea5e54ce3ef845ff7ebc15c8f
Joseph <joseph42@live.co.uk>
2019-12-28 16:54:48 +0000

    Add PUT & HEAD

--- async.lua
+++ async.lua
@@ -1,5 +1,6 @@
 local counter = 0
 
+-- GET
 local function Async_Get(url, headers)
   if url == nil then return end
   if headers == nil then headers = {} end
@@ -17,12 +18,13 @@ local function Async_GetParams(url, headers, params)
   if params == nil then params = {} end
 
   counter = counter + 1
-  CallJavaStaticMethod(JVM, 'dev/joseph/http/Async', 'get', '(Ljava/lang/Integer;Ljava/lang/String;Ljava/util/Map;)V', counter, url, headers)
+  CallJavaStaticMethod(JVM, 'dev/joseph/http/Async', 'get', '(Ljava/lang/Integer;Ljava/lang/String;Ljava/util/Map;Ljava/util/Map;)V', counter, url, headers, params)
 
   return counter
 end
 AddFunctionExport('GetAsync', Async_GetParams)
 
+-- POST
 local function Async_Post(url, headers, body)
   if url == nil then return end
   if headers == nil then headers = {} end
@@ -46,3 +48,52 @@ local function Async_PostFields(url, headers, fields)
   return counter
 end
 AddFunctionExport('PostAsync', Async_PostFields)
+
+-- PUT
+local function Async_Put(url, headers, body)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+  if body == nil then body = "" end
+
+  counter = counter + 1
+  CallJavaStaticMethod(JVM, 'dev/joseph/http/Async', 'put', '(Ljava/lang/Integer;Ljava/lang/String;Ljava/util/Map;Ljava/lang/String;)V', counter, url, headers, body)
+
+  return counter
+end
+AddFunctionExport('PutAsync', Async_Put)
+
+local function Async_PutFields(url, headers, fields)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+  if fields == nil then fields = {} end
+
+  counter = counter + 1
+  CallJavaStaticMethod(JVM, 'dev/joseph/http/Async', 'put', '(Ljava/lang/Integer;Ljava/lang/String;Ljava/util/Map;Ljava/util/Map;)V', counter, url, headers, fields)
+
+  return counter
+end
+AddFunctionExport('PutAsync', Async_PutFields)
+
+-- HEAD
+local function Async_Head(url, headers)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+
+  counter = counter + 1
+  CallJavaStaticMethod(JVM, 'dev/joseph/http/Async', 'head', '(Ljava/lang/Integer;Ljava/lang/String;Ljava/util/Map;)V', counter, url, headers)
+
+  return counter
+end
+AddFunctionExport('HeadAsync', Async_Head)
+
+local function Async_HeadParams(url, headers, params)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+  if params == nil then params = {} end
+
+  counter = counter + 1
+  CallJavaStaticMethod(JVM, 'dev/joseph/http/Async', 'head', '(Ljava/lang/Integer;Ljava/lang/String;Ljava/util/Map;Ljava/util/Map;)V', counter, url, headers, params)
+
+  return counter
+end
+AddFunctionExport('HeadAsync', Async_HeadParams)
--- sync.lua
+++ sync.lua
@@ -1,3 +1,4 @@
+-- GET
 local function Sync_Get(url, headers)
   if url == nil then return end
   if headers == nil then headers = {} end
@@ -21,6 +22,7 @@ local function Sync_GetParams(url, headers, params)
 end
 AddFunctionExport('Get', Sync_Get)
 
+-- POST
 local function Sync_Post(url, headers, body)
   if url == nil then return end
   if headers == nil then headers = {} end
@@ -44,3 +46,52 @@ local function Sync_PostFields(url, headers, fields)
   return _response
 end
 AddFunctionExport('Post', Sync_PostFields)
+
+-- PUT
+local function Sync_Put(url, headers, body)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+  if body == nil then body = "" end
+
+  local _response = CallJavaStaticMethod(JVM, 'dev/joseph/http/Sync', 'put', '(Ljava/lang/String;Ljava/util/Map;Ljava/lang/String;)Ljava/util/Map;', url, headers, body)
+
+  if _response == nil then return false end
+  return _response
+end
+AddFunctionExport('Put', Sync_Put)
+
+local function Sync_PutFields(url, headers, fields)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+  if fields == nil then fields = {} end
+
+  local _response = CallJavaStaticMethod(JVM, 'dev/joseph/http/Sync', 'put', '(Ljava/lang/String;Ljava/util/Map;Ljava/util/Map;)Ljava/util/Map;', url, headers, fields)
+
+  if _response == nil then return false end
+  return _response
+end
+AddFunctionExport('Put', Sync_PutFields)
+
+-- HEAD
+local function Sync_Head(url, headers)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+
+  local _response = CallJavaStaticMethod(JVM, 'dev/joseph/http/Sync', 'head', '(Ljava/lang/String;Ljava/util/Map;)Ljava/util/Map;', url, headers)
+
+  if _response == nil then return false end
+  return _response
+end
+AddFunctionExport('Head', Sync_Head)
+
+local function Sync_HeadParams(url, headers, params)
+  if url == nil then return end
+  if headers == nil then headers = {} end
+  if params == nil then params = {} end
+
+  local _response = CallJavaStaticMethod(JVM, 'dev/joseph/http/Sync', 'head', '(Ljava/lang/String;Ljava/util/Map;Ljava/util/Map;)Ljava/util/Map;', url, headers, params)
+
+  if _response == nil then return false end
+  return _response
+end
+AddFunctionExport('Head', Sync_HeadParams)
